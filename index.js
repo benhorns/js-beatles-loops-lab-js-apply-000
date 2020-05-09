1c7f505e2349aa3867a1022565c1c4a75c2deff6
Benjamin Horns <benhorns@gmail.com>
2020-05-09 13:04:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay (muscians, instruments) {
 const length = muscians.length-1
 let instrumentsp = [];
   for(let i = 0; i < length; i++) {
-  band.push(`${muscians[i]} plays ${instrumentsp[i]}`);
+  instrumentsp.push(`${muscians[i]} plays ${instrumentsp[i]}`);
   }
   return band;
 }
