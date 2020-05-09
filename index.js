f12ea449524ad3962d16f4c61981ce1602080b63
Benjamin Horns <benhorns@gmail.com>
2020-05-09 13:04:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay (muscians, instruments) {
 const length = muscians.length-1
-let band = [];
+let instrumentsp = [];
   for(let i = 0; i < length; i++) {
   band.push(`${muscians[i]} plays ${instrumentsp[i]}`);
   }
